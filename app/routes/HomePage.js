dc1f8b85bbf2df7ef4434f4317e8675058a16a15
심은지 <sim-eunji@sim-eunjiui-MacBook-Pro-3.local>
2018-08-25 16:47:54 +0900

    save master.

--- app/routes/HomePage.js
+++ app/routes/HomePage.js
@@ -18,12 +18,29 @@ class HomePage extends Component {
         <div style={{ color: "#6b6161" }}>
           <header>
             <Row>
-              <Column col={6}>
-                <div style={{ height: "180px", paddingTop: "70px" }}>
-                  <img
-                    src="https://github.com/eunjishim/ej_shim/blob/master/app/assets/Oval%203.png?raw=true"
-                    style={{ width: "40px" }}
-                  />
+              <Column col={12} style={{ backgroundColor: "#d18484" }}>
+                <div
+                  style={{
+                    height: "180px",
+                    paddingTop: "70px",
+                    paddingBottom: "70px"
+                  }}
+                >
+                  <Row>
+                    <Column col={7} style={{ backgroundColor: "#ffffff" }}>
+                      <div style={{ paddingLeft: "40px" }}>
+                        <img
+                          src="https://github.com/eunjishim/ej_shim/blob/master/app/assets/Oval%203.png?raw=true"
+                          style={{ width: "40" }}
+                        />
+                      </div>
+                    </Column>
+                    <Column col={1} style={{ backgroundColor: "#ffffff" }}>
+                      <div style={{ width: "100px" }}>
+                        <a href="/work"><span>Work</span></a>
+                      </div>
+                    </Column>
+                  </Row>
                 </div>
               </Column>
             </Row>
